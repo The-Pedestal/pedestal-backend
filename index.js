044187b31e279b9144fe6938bdb9d335cf8fcf08
Jimmy <jimmybatuhan@gmail.com>
2021-08-20 23:15:46 +0800

    Added async

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const User = require('./models/User.js');
 
-module.exports.connect = () => {
+module.exports.connect = async () => {
     await mongoose.connect('mongodb+srv://pedestalapp:FKA0Z7ajvRdRC7Ez@cluster0.ib7jt.mongodb.net/pedestal?retryWrites=true&w=majority', {
         useNewUrlParser: true,
         useUnifiedTopology: true,
