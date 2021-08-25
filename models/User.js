d721c8f5a5220fc745038d5e0213b46237f72936
kjschelling <kjschelling@gmail.com>
2021-08-25 13:58:53 -0700

    added experience & pronouns to User schema

--- models/User.js
+++ models/User.js
@@ -1,23 +1,28 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    first_name: String,
-    last_name: String,
-    birth_date: Date,
-    gender: String,
-    occupation: String,
-    email: String,
-    username: String,
-    phone: String,
-    photo: String,
-    getstream_token: String,
-    cognito_sub: String,
-    getstream_user_id: String,
-    connections: [Object],
-    interests: [String],
-    tagline: String,
-    bio: String,
-    brand_statement: String
+	first_name: String,
+	last_name: String,
+	birth_date: Date,
+	gender: String,
+	occupation: String,
+	email: String,
+	username: String,
+	phone: String,
+	photo: String,
+	getstream_token: String,
+	cognito_sub: String,
+	getstream_user_id: String,
+	connections: [Object],
+	interests: [String],
+	tagline: String,
+	bio: String,
+	brand_statement: String,
+	experiences: [Object],
+	pronouns: String,
 });
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model(
+	'User',
+	UserSchema
+);
