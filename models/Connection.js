19aba188a10a5492f5c6e4a1abd3a0daeaf726d9
Jimmy <jimmybatuhan@gmail.com>
2021-08-25 23:11:04 +0800

    updated columns for connections docs

--- models/Connection.js
+++ models/Connection.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const ConnectionSchema = new mongoose.Schema({
-    user_id: String,
-    connected_user: String,
+    cognito_sub: String,
+    connected_cognito_sub: String,
     status: String
 });
 
