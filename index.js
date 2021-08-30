a0e2cc440692270f529c4dd0c77d85618d5c35fd
Jimmy <jimmybatuhan@gmail.com>
2021-08-31 00:34:34 +0800

    export user experience

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const User = require('./models/User.js');
 const Connection = require('./models/Connection.js');
 const Interests = require('./models/Interest.js');
 const CONNECTION_STATUSES = require('./constants/ConnectionStatuses.js');
+const UserExperience = require('./models/UserExperience.js');
 
 module.exports.connect = async (user, password, cluster, database) => {
     await mongoose.connect(`mongodb+srv://${user}:${password}@${cluster}/${database}?retryWrites=true&w=majority`, {
@@ -16,6 +17,7 @@ module.exports.connect = async (user, password, cluster, database) => {
 module.exports.User = User;
 module.exports.Connection = Connection;
 module.exports.Interests = Interests;
+module.exports.UserExperience = UserExperience;
 
 module.exports.CONSTANTS = {
     CONNECTION_STATUSES: CONNECTION_STATUSES
