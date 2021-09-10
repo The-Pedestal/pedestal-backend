fa7e4433c637484d930e6b7741748fe3007f1a80
Jimmy <jimmybatuhan@gmail.com>
2021-09-10 23:34:56 +0800

    udpated mentorship update function

--- controllers/MentorshipController.js
+++ controllers/MentorshipController.js
@@ -32,50 +32,6 @@ module.exports.showUserMentors = async (req, res) => {
     res.send(result);
 }
 
-module.exports.updateAgreementStatus = async (req, res) => {
-    const result = {};
-    const user_id = req.params.user;
-    const status = req.body.status;
-    const mentorship_id = req.params.id;
-    const mentorship = await Models.Mentorship.findOne({
-        _id: mentorship_id
-    });
-
-    switch (true) {
-        case mentorship.mentee.equals(user_id):
-            mentorship.mentee_agreement_status = status;
-            break;
-        case mentorship.mentor.equals(user_id):
-            mentorship.mentor_agreement_status = status;
-            break;
-        default:
-            throw 'user is not in this mentorship program'
-            break;
-    }
-
-    if (mentorship.mentor_agreement_status === AppConstants.MENTOR_AGREEMENT_ACCEPTED &&
-        mentorship.mentee_agreement_status === AppConstants.MENTEE_AGREEMENT_ACCEPTED) {
-        mentorship.status = AppConstants.MENTORSHIP_ACCEPTED;
-    }
-
-    if (mentorship.mentor_agreement_status === AppConstants.MENTOR_AGREEMENT_PENDING ||
-        mentorship.mentee_agreement_status === AppConstants.MENTEE_AGREEMENT_PENDING) {
-        mentorship.status = AppConstants.MENTORSHIP_PENDING;
-    }
-
-    if (mentorship.mentor_agreement_status === AppConstants.MENTOR_AGREEMENT_DECLINED ||
-        mentorship.mentee_agreement_status === AppConstants.MENTEE_AGREEMENT_DECLINED) {
-        mentorship.status = AppConstants.MENTORSHIP_DECLINED;
-    }
-
-    const updated_mentorship = await mentorship.save();
-
-    result.success = true;
-    result.data = updated_mentorship;
-
-    res.send(result);
-}
-
 module.exports.getUserMentees = async (req, res) => {
     const result = {}
     const mentors = await Models.Mentorship.find({
@@ -215,25 +171,39 @@ module.exports.update = async (req, res) => {
     const result = {};
     const user = req.body.user;
     const role = req.body.role;
-    const filter = {
-        _id: req.params.id
-    };
-    const update = {};
+    const mentorship = await Models.Mentorship.findOne({
+        _id: req.params.id,
+        ...(role === AppConstants.MENTOR && { mentor: user }),
+        ...(role === AppConstants.MENTEE && { mentee: user }),
+    });
 
     switch (role) {
         case AppConstants.MENTOR:
-            filter.mentor = user;
-            update.mentor_agreement_status = req.body.agreement_status;
+            mentorship.mentor_agreement_status = req.body.agreement_status;
             break;
         case AppConstants.MENTEE:
-            filter.mentee = user;
-            update.mentee_agreement_status = req.body.agreement_status;
+            mentorship.mentee_agreement_status = req.body.agreement_status;
             break;
         default:
             throw 'can\'t failed to identify the user\'s role';
     }
 
-    const mentorship = await Models.Mentorship.findOneAndUpdate(filter, update, { returnOriginal: false });
+    if (mentorship.mentor_agreement_status === AppConstants.MENTOR_AGREEMENT_ACCEPTED &&
+        mentorship.mentee_agreement_status === AppConstants.MENTEE_AGREEMENT_ACCEPTED) {
+        mentorship.status = AppConstants.MENTORSHIP_ACCEPTED;
+    }
+
+    if (mentorship.mentor_agreement_status === AppConstants.MENTOR_AGREEMENT_PENDING ||
+        mentorship.mentee_agreement_status === AppConstants.MENTEE_AGREEMENT_PENDING) {
+        mentorship.status = AppConstants.MENTORSHIP_PENDING;
+    }
+
+    if (mentorship.mentor_agreement_status === AppConstants.MENTOR_AGREEMENT_DECLINED ||
+        mentorship.mentee_agreement_status === AppConstants.MENTEE_AGREEMENT_DECLINED) {
+        mentorship.status = AppConstants.MENTORSHIP_DECLINED;
+    }
+
+    await mentorship.save();
 
     result.success = true;
     result.data = mentorship;
