6211192b93818ff53915c596bdc383aa2e697e0f
kjschelling <kjschelling@gmail.com>
2021-09-10 11:07:49 -0700

    changed prettier formatting

--- models/index.js
+++ models/index.js
@@ -1,56 +1,29 @@
-const InterestSchema = require('./schemas/Interest.js');
-const mongoose = require('mongoose');
-const UserConnectionSchema = require('./schemas/UserConnection.js');
-const UserExperienceSchema = require('./schemas/UserExperience.js');
-const UserSchema = require('./schemas/User.js');
-const UserEducationSchema = require('./schemas/UserEducation.js');
-const UserProjectSchema = require('./schemas/UserProject.js');
-const WorkspaceSchema = require('./schemas/Workspace.js');
-const AchievementsSchema = require('./schemas/Achievements.js');
-const MentorshipSchema = require('./schemas/Mentorship.js');
-const WorkspaceScheduleSchema = require('./schemas/WorkspaceSchedule');
-const UserActivitySchema = require('./schemas/UserActivity');
+const InterestSchema = require("./schemas/Interest.js");
+const mongoose = require("mongoose");
+const UserConnectionSchema = require("./schemas/UserConnection.js");
+const UserExperienceSchema = require("./schemas/UserExperience.js");
+const UserSchema = require("./schemas/User.js");
+const UserEducationSchema = require("./schemas/UserEducation.js");
+const UserProjectSchema = require("./schemas/UserProject.js");
+const WorkspaceSchema = require("./schemas/Workspace.js");
+const AchievementsSchema = require("./schemas/Achievements.js");
+const MentorshipSchema = require("./schemas/Mentorship.js");
+const WorkspaceScheduleSchema = require("./schemas/WorkspaceSchedule");
+const UserActivitySchema = require("./schemas/UserActivity");
 
 module.exports = {
-	UserEducation: mongoose.model(
-		'user_education',
-		UserEducationSchema
-	),
-	UserProject: mongoose.model(
-		'user_project',
-		UserProjectSchema
-	),
-	Achievement: mongoose.model(
-		'achievement',
-		AchievementsSchema
-	),
-	Interest: mongoose.model(
-		'interest',
-		InterestSchema
-	),
-	Mentorship: mongoose.model(
-		'mentorship',
-		MentorshipSchema
-	),
-	User: mongoose.model('user', UserSchema),
-	UserActivity: mongoose.model(
-		'user_activities',
-		UserActivitySchema
-	),
-	UserConnection: mongoose.model(
-		'user_connection',
-		UserConnectionSchema
-	),
-	UserExperience: mongoose.model(
-		'user_experience',
-		UserExperienceSchema
-	),
-	Workspace: mongoose.model(
-		'workspace',
-		WorkspaceSchema
-	),
-	WorkspaceSchedule: mongoose.model(
-		'workspace_schedule',
-		WorkspaceScheduleSchema
-	),
+  UserEducation: mongoose.model("user_education", UserEducationSchema),
+  UserProject: mongoose.model("user_project", UserProjectSchema),
+  Achievement: mongoose.model("achievement", AchievementsSchema),
+  Interest: mongoose.model("interest", InterestSchema),
+  Mentorship: mongoose.model("mentorship", MentorshipSchema),
+  User: mongoose.model("user", UserSchema),
+  UserActivity: mongoose.model("user_activities", UserActivitySchema),
+  UserConnection: mongoose.model("user_connection", UserConnectionSchema),
+  UserExperience: mongoose.model("user_experience", UserExperienceSchema),
+  Workspace: mongoose.model("workspace", WorkspaceSchema),
+  WorkspaceSchedule: mongoose.model(
+    "workspace_schedule",
+    WorkspaceScheduleSchema
+  ),
 };
--- utils/Connections.js
+++ utils/Connections.js
@@ -1,18 +1,18 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 module.exports.connect = async () => {
-	const user = process.env.MONGODB_USER;
-	const password = process.env.MONGODB_PASSWORD;
-	const cluster = process.env.MONGODB_CLUSTER;
-	const database = process.env.MONGODB_DATABASE;
+  const user = process.env.MONGODB_USER;
+  const password = process.env.MONGODB_PASSWORD;
+  const cluster = process.env.MONGODB_CLUSTER;
+  const database = process.env.MONGODB_DATABASE;
 
-	await mongoose.connect(
-		`mongodb+srv://${user}:${password}@${cluster}/${database}?retryWrites=true&w=majority`,
-		{
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-			useFindAndModify: false,
-			useCreateIndex: true,
-		}
-	);
+  await mongoose.connect(
+    `mongodb+srv://${user}:${password}@${cluster}/${database}?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    }
+  );
 };
