83b86daee266f6395c756a82d741b84911e1cd5a
Jimmy <jimmybatuhan@gmail.com>
2021-09-22 02:32:32 +0800

    renamed params

--- routes/Activities.js
+++ routes/Activities.js
@@ -6,7 +6,7 @@ module.exports.init = async (app) => {
     ConnectionUtil.connect();
 
     app.get('/activities', ActivityController.get);
-    app.get('/activities/:activity_id', ActivityController.show);
+    app.get('/activities/:id', ActivityController.show);
     app.post('/activities/:activity_id/reactions', ActivityController.createReaction)
     app.put('/activities/:activity_id/reactions/:id', ActivityController.updateReaction);
     app.delete('/activities/:activity_id/reactions/:id', ActivityController.deleteReaction);
