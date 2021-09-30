4e505fe1e25a7d6cf4b0f74287292d1a662bc508
kjschelling <kjschelling@gmail.com>
2021-09-30 13:13:13 -0700

    updated user schema to have is_currently_mentoring & is_opt_out_of_mentoring attributes

--- models/schemas/User.js
+++ models/schemas/User.js
@@ -1,111 +1,113 @@
 const mongoose = require('mongoose');
 const SocialMediaSchema = require('./subdocuments/SocialMedia');
 
-const UserSchema = new mongoose.Schema({
-    first_name: {
-        type: String,
-        required: true,
-    },
-    last_name: {
-        type: String,
-        required: true,
-    },
-    date_of_birth: {
-        type: String,
-        default: null, //should always be in MM/DD/YYYY format
-    },
-    gender: {
-        type: String,
-        default: null,
-        enum: {
-            values: [null, 'Male', 'Female', 'Other'],
+const UserSchema = new mongoose.Schema(
+    {
+        first_name: {
+            type: String,
+            required: true,
         },
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    username: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    phone: {
-        type: String,
-        required: true, //should always be in E.164 format
-    },
-    photo: {
-        type: String,
-        default: null,
-    },
-    cognito_sub: {
-        type: String,
-        default: null,
-    },
-    getstream_token: {
-        type: String,
-        default: null,
-    },
-    tagline: {
-        type: String,
-        default: null,
-    },
-    bio: {
-        type: String,
-        default: null,
-    },
-    pronouns: {
-        type: String,
-        default: null,
-    },
-    location: {
-        type: String,
-        default: null,
-    },
-    brand_statement: {
-        type: String,
-        default: null,
-    },
-    is_mentor: Boolean,
-    is_mentee: Boolean,
-    skills: [String],
-    workspaces: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'workspace',
+        last_name: {
+            type: String,
+            required: true,
         },
-    ],
-    interests: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'interest',
+        date_of_birth: {
+            type: String,
+            default: null, //should always be in MM/DD/YYYY format
         },
-    ],
-    experiences: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'user_experience',
+        gender: {
+            type: String,
+            default: null,
+            enum: {
+                values: [null, 'Male', 'Female', 'Other'],
+            },
         },
-    ],
-    projects: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'user_project',
+        email: {
+            type: String,
+            required: true,
+            unique: true,
         },
-    ],
-    education: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'user_education',
+        username: {
+            type: String,
+            required: true,
+            unique: true,
         },
-    ],
-    social_media: SocialMediaSchema,
-},  { timestamps: true });
+        phone: {
+            type: String,
+            required: true, //should always be in E.164 format
+        },
+        photo: {
+            type: String,
+            default: null,
+        },
+        cognito_sub: {
+            type: String,
+            default: null,
+        },
+        getstream_token: {
+            type: String,
+            default: null,
+        },
+        tagline: {
+            type: String,
+            default: null,
+        },
+        bio: {
+            type: String,
+            default: null,
+        },
+        pronouns: {
+            type: String,
+            default: null,
+        },
+        location: {
+            type: String,
+            default: null,
+        },
+        brand_statement: {
+            type: String,
+            default: null,
+        },
+        is_currently_mentoring: Boolean,
+        is_opt_out_mentoring: Boolean,
+        skills: [String],
+        workspaces: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'workspace',
+            },
+        ],
+        interests: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'interest',
+            },
+        ],
+        experiences: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'user_experience',
+            },
+        ],
+        projects: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'user_project',
+            },
+        ],
+        education: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'user_education',
+            },
+        ],
+        social_media: SocialMediaSchema,
+    },
+    { timestamps: true },
+);
 
-UserSchema.virtual('full_name').get(
-    function () {
-        return this.first_name + ' ' + this.last_name;
-    });
+UserSchema.virtual('full_name').get(function () {
+    return this.first_name + ' ' + this.last_name;
+});
 
 module.exports = UserSchema;
