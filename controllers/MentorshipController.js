0b1aa849c96cab6c2ea6572e70b4bf5f353cde15
kjschelling <kjschelling@gmail.com>
2021-10-27 09:19:59 -0700

    fixed bug in mentorships controller on create, changed packeage version to 3.7.7

--- controllers/MentorshipController.js
+++ controllers/MentorshipController.js
@@ -129,42 +129,58 @@ module.exports.updateMentorshipAgreementStatus = async (req, res) => {
 
 module.exports.create = async (req, res) => {
     const result = {};
-    const user = req.body.mentee;
-    const mentor_id = req.body.mentor;
+    const user = req.body.user;
+    const applying_as = req.body.as;
+    const applying_to = req.body.to;
     const new_mentorship = {
         applicant: user,
-        mentor: mentor_id,
-        mentee: user,
-        status: AppConstants.MENTORSHIP_PENDING
+        mentor: null,
+        mentee: null,
     };
+    /** apply the '@var user' as either mentor or mentee */
+    switch (applying_as) {
+        case AppConstants.MENTEE:
+            new_mentorship.mentor = user;
+            new_mentorship.mentee = applying_to;
+            /** set the agreement status of the applicant to pending */
+            new_mentorship.mentor_agreement_status = AppConstants.MENTOR_AGREEMENT_PENDING;
+            break;
+        case AppConstants.MENTOR:
+            new_mentorship.mentor = applying_to;
+            new_mentorship.mentee = user;
+            /** set the agreement status of the applicant to pending */
+            new_mentorship.mentee_agreement_status = AppConstants.MENTEE_AGREEMENT_PENDING;
+            break;
+        default:
+            throw 'failed - you or mentor may have outed out of mentoring, message mentor or check user settings';
+    }
+
     const applicant = await Models.User.findById(user);
-    const mentor = await Models.User.findById(mentor_id);
+    const mentor = await Models.User.findById(new_mentorship.mentor);
 
     if (mentor.is_opt_out_mentoring) {
         return res.status(500).send({
             success: false,
-            error: 'mentor opted out of mentoring'
+            error: 'mentor opted out of mentoring',
         });
     }
 
+    //getstream notification
     Models.Mentorship.create(new_mentorship, async (error, mentorship) => {
         try {
-            await gs_client.feed("notifications", mentor_id).addActivity({
+            await gs_client.feed('notifications', mentor._id).addActivity({
                 actor: await gs_client.user(user).get(),
                 verb: `mentorship_request`,
-                object: await gs_client.collections.add(
-                    "mentorship",
-                    null,
-                    {
-                        mentorship_id: mentorship._id,
-                        message: `${applicant.full_name} ${!mentor.is_currently_mentoring
+                object: await gs_client.collections.add('mentorship', null, {
+                    mentorship_id: mentorship._id,
+                    message: `${applicant.full_name} ${
+                        !mentor.is_currently_mentoring
                             ? 'is requesting you to be their mentor'
-                            : 'wants to book you as a mentor'}`,
-                        link: `/mentorships`,
-                    }
-                ),
+                            : 'wants to book you as a mentor'
+                    }`,
+                    link: `/mentorships`,
+                }),
             });
-
             res.status(200);
             result.success = true;
             result.data = mentorship;
@@ -199,25 +215,21 @@ module.exports.update = async (req, res) => {
                 break;
         }
 
-        await gs_client.feed("notifications", mentorship.mentee).addActivity({
+        await gs_client.feed('notifications', mentorship.mentee).addActivity({
             actor: await gs_client.user(mentorship.mentor).get(),
             verb: `mentorship_request`,
-            object: await gs_client.collections.add(
-                "mentorship",
-                null,
-                {
-                    mentorship_id: mentorship._id,
-                    message: message,
-                    link: `/mentorships`,
-                }
-            ),
+            object: await gs_client.collections.add('mentorship', null, {
+                mentorship_id: mentorship._id,
+                message: message,
+                link: `/mentorships`,
+            }),
         });
     }
 
     result.success = true;
     result.data = mentorship;
 
-    res.send(result);;
+    res.send(result);
 };
 
 module.exports.delete = async (req, res) => {
